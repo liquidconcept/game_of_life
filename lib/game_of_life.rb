314ba183771da8937df2fc0e507987eaab550239
jujudellago <julien.ramel@gmail.com>
2011-04-18 18:15:02 +0200

    basic tests passes

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -4,5 +4,22 @@
 #
 module GameOfLife
   class Game
+    def initialize(board="")
+      @board=board
+    end
+    def run(nb=1)
+      r=""
+      nb.times do
+        r+=@board
+      end
+      return r
+    end
+    
+    def board
+      @board
+    end
+    def to_s
+      @board
+    end
   end
 end
